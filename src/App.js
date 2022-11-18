02c6bdca498e2eba7b56d816a2ddd77d6f14bd34
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-18 13:03:26 +0100

    Working header in AllBeers page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
+import AllBeers from './components/AllBeers';
+import RandomBeer from './components/RandomBeer';
+import NewBeer from './components/NewBeer';
 import {useEffect, useState} from 'react';
 
 
@@ -9,6 +12,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/beers' element={<AllBeers />} />
+        <Route path='/random-beer' element={<RandomBeer />} />
+        <Route path='/new-beer' element={<NewBeer />} />
       </Routes>
     </div>
   );
---
+++ src/components/AllBeers.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function AllBeers() {
+    return (
+        <header className="Header">
+            <Link to={'/'}>Home</Link>
+        </header>
+    )
+}
+
+export default AllBeers;
