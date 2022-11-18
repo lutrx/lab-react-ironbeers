ed37f8c79750e0c9cd4a6ea25f624ad539b41f2c
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-18 14:04:15 +0100

    Completed iteration2

---
+++ src/components/NewBeer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function NewBeer() {
+    return (
+        <header className="Header">
+            <Link to={'/'}>Home</Link>
+        </header>
+    )
+}
+
+export default NewBeer;
---
+++ src/components/RandomBeer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function RandomBeer() {
+    return (
+        <header className="Header">
+            <Link to={'/'}>Home</Link>
+        </header>
+    )
+}
+
+export default RandomBeer;
